6f338cf62579e94f2b89f5d32c03e00086f32cbf
Vivekdu07 <vivekchoubeykmcdu@gmail.com>
2026-06-24 16:02:44 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,23 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        slate: {
+          950: '#020617',
+          900: '#0f172a',
+          800: '#1e293b',
+          300: '#cbd5e1',
+          100: '#f1f5f9',
+        },
+        red: {
+          500: '#ef4444',
+        }
+      }
+    },
   },
   plugins: [],
 }
